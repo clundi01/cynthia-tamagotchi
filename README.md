0bbede4ac71d9e351cf42ea0d3c1389872621ae0
clundi01 <clundi01@github.com>
2022-03-01 15:56:33 -0500

    Made my title and centered it

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cynthia-tamagotchi
+
+Thanks for taking a look at my code for Tamagotchi
---
+++ js/main.js
@@ -0,0 +1,20 @@
+// //console.log("Is this connected");
+
+// const myButton = document.getElementById ("my-button");
+// //console.log(myButton);
+
+// const myCount =document.getElementById("my-count")
+
+// document.getElementById("my-count");
+// //console.log(myCount);
+
+// //need an Event listener
+
+// const addOne = () => {
+//     //console.log("addOne function invoked")
+//     myCount
+// };
+
+
+// myButton.addEventListener('click', addOne);
+
