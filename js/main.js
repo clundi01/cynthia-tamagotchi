5e449bd3ea2e3cbac9c18fde4d7dec3a754f4d22
clundi01 <98920219+clundi01@users.noreply.github.com>
2022-03-04 10:10:32 -0500

    Functions
    I added the if functions to begin with my buttons

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-let feed = 10;
-let sleep = 10;
+let feed = 100;
+let sleep = 100;
 let play = 0;
 
 function newGame() {
-feed = 10;
-sleep = 10;
+feed = 100;
+sleep = 100;
 play = 0;
 }
+
+function deathCheck() {
+  if (feed > 100) {
+    alert("Your pet has died of hunger!");
+    newGame();
+  }
+  
+  if (sleep < 0) {
+    alert("Your pet has died of fatigue!");
+    newGame();
+  }
+  
+  if (play < 0) {
+    alert("Your pet has died of bordem!");
+    newGame();
+  }
+  }
+  
