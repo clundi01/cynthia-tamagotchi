a25e372b748b6ba0248320b0dc1b31a1ea86021b
clundi01 <98920219+clundi01@users.noreply.github.com>
2022-03-06 15:00:47 -0500

    added in JS
    added new functions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
-// //setInterval(function(){
-//   console.log(count++);
-// }, 1000);
-
+// Added counter for each functions as below
+let agePet = 0
+let hunger = 0
+let bore = 0
+let sleep = 0
 let count = 0
+
+// calls each function when the page loads
+window.onload = function main() {
+  ageCount();
+  hungerCount();
+  boreCount();
+  sleepCount();
+}
+//added each variable for the set interval function so we can stop the timer
+// according to the condition we set
+let ageCounter = null;
+let hungerCounter = null
+let boreCounter = null
+let sleepCounter = null
 letmyCounter = null;
 
 // Call the provided function every 1000 milliseconds
